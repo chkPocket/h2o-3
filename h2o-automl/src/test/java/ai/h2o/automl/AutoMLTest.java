0d59b6a6fbfdb3ea96003fa10db4bf5a40e511ff
Navdeep Gill <navdeep@h2o.ai>
2017-06-05 17:59:22 -0700

    xgb+automl

--- h2o-automl/src/test/java/ai/h2o/automl/AutoMLTest.java
+++ h2o-automl/src/test/java/ai/h2o/automl/AutoMLTest.java
@@ -11,7 +11,7 @@ public class AutoMLTest extends TestUtil {
 
   @BeforeClass public static void setup() { stall_till_cloudsize(1); }
 
-  @Test public void AirlinesTest() {
+  @Test public void airlinesTest() {
     AutoML aml=null;
     Frame fr=null;
     try {
@@ -20,7 +20,73 @@ public class AutoMLTest extends TestUtil {
       autoMLBuildSpec.input_spec.training_frame = fr._key;
       autoMLBuildSpec.input_spec.response_column = "IsDepDelayed";
       autoMLBuildSpec.build_control.loss = "AUTO";
-      autoMLBuildSpec.build_control.stopping_criteria.set_max_runtime_secs(5);
+      autoMLBuildSpec.build_control.stopping_criteria.set_max_runtime_secs(20);
+
+      aml = AutoML.makeAutoML(Key.<AutoML>make(), new Date(), autoMLBuildSpec);
+      AutoML.startAutoML(aml);
+      aml.get();
+
+    } finally {
+      // cleanup
+      if(aml!=null) aml.deleteWithChildren();
+      if(fr != null) fr.remove();
+    }
+  }
+
+  @Test public void weatherTest() {
+    AutoML aml=null;
+    Frame fr=null;
+    try {
+      AutoMLBuildSpec autoMLBuildSpec = new AutoMLBuildSpec();
+      fr = parse_test_file("./smalldata/junit/weather.csv");
+      autoMLBuildSpec.input_spec.training_frame = fr._key;
+      autoMLBuildSpec.input_spec.response_column = "RainTomorrow";
+      autoMLBuildSpec.build_control.loss = "AUTO";
+      autoMLBuildSpec.build_control.stopping_criteria.set_max_runtime_secs(20);
+
+      aml = AutoML.makeAutoML(Key.<AutoML>make(), new Date(), autoMLBuildSpec);
+      AutoML.startAutoML(aml);
+      aml.get();
+
+    } finally {
+      // cleanup
+      if(aml!=null) aml.deleteWithChildren();
+      if(fr != null) fr.remove();
+    }
+  }
+
+  @Test public void carsTest() {
+    AutoML aml=null;
+    Frame fr=null;
+    try {
+      AutoMLBuildSpec autoMLBuildSpec = new AutoMLBuildSpec();
+      fr = parse_test_file("./smalldata/junit/cars.csv");
+      autoMLBuildSpec.input_spec.training_frame = fr._key;
+      autoMLBuildSpec.input_spec.response_column = "economy (mpg)";
+      autoMLBuildSpec.build_control.loss = "AUTO";
+      autoMLBuildSpec.build_control.stopping_criteria.set_max_runtime_secs(20);
+
+      aml = AutoML.makeAutoML(Key.<AutoML>make(), new Date(), autoMLBuildSpec);
+      AutoML.startAutoML(aml);
+      aml.get();
+
+    } finally {
+      // cleanup
+      if(aml!=null) aml.deleteWithChildren();
+      if(fr != null) fr.remove();
+    }
+  }
+
+  @Test public void irisTest() {
+    AutoML aml=null;
+    Frame fr=null;
+    try {
+      AutoMLBuildSpec autoMLBuildSpec = new AutoMLBuildSpec();
+      fr = parse_test_file("./smalldata/iris/iris.csv");
+      autoMLBuildSpec.input_spec.training_frame = fr._key;
+      autoMLBuildSpec.input_spec.response_column = fr.lastVecName();
+      autoMLBuildSpec.build_control.loss = "AUTO";
+      autoMLBuildSpec.build_control.stopping_criteria.set_max_runtime_secs(20);
 
       aml = AutoML.makeAutoML(Key.<AutoML>make(), new Date(), autoMLBuildSpec);
       AutoML.startAutoML(aml);
